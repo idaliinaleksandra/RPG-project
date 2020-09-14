3df0b7e272f2afdba5986988a0cb0617397fb386
idaliina <25195407+idaliinaleksandra@users.noreply.github.com>
2020-09-14 14:32:07 +0300

    WIP: saving and loadin

---
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,51 @@
+﻿using RPG.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Saving { 
+    [ExecuteAlways]
+    public class SaveableEntity : MonoBehaviour
+    {
+        [SerializeField] string uniqueID = "";
+
+        public string GetUniqueIdentifier()
+        {
+            return uniqueID;
+        }
+
+        public object CaptureState()
+        {
+            return new SerializableVector3(transform.position);
+        }
+
+        public void RestoreState(object state)
+        {
+            SerializableVector3 pos = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = pos.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            SerializedObject obj = new SerializedObject(this);
+            SerializedProperty property = obj.FindProperty("uniqueID");
+
+            print("Editing");
+            if (string.IsNullOrEmpty(property.stringValue))
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                obj.ApplyModifiedProperties();
+            }
+        }
+#endif
+    }
+}
